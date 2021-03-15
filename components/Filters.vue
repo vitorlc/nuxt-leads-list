238661eeada9391ac767d16409f00c24c764e139
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-15 19:52:34 -0300

    Add Filter component

---
+++ components/Filters.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label>Nome:</label>
+    <input v-model="name"/>
+    <label >Categorias</label>
+    <select v-model="category">
+      <option 
+        v-for="(option, index) in categoryOptions"
+        :key="index"
+        :value="option">{{option}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    categoryOptions: Array
+  },
+  watch: {
+    name: function (value) {
+      this.$emit('changeName', value)
+    }
+  },
+  data () {
+    return {
+      name: '',
+      category: null
+    }
+  }
+};
+</script>
+
+<style>
+</style>
