3d0bad89a1877dcdba86b914714d348c7e4afea1
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-15 21:25:01 -0300

    Refactoring Filter component

--- components/Filters.vue
+++ components/Filters.vue
@@ -1,17 +1,22 @@
 <template>
-  <div>
-    <label>Nome:</label>
-    <input v-model="filter.name" />
-    <label>Categorias</label>
-    <select v-model="filter.category">
-      <option
-        v-for="(option, index) in categoryOptions"
-        :key="index"
-        :value="option"
-      >
-        {{ option }}
-      </option>
-    </select>
+  <div class="container">
+    <div class="filter">
+      <label>Nome:</label>
+      <input v-model="filter.name" />
+    </div>
+    <div class="filter">
+      <label>Categorias: </label>
+      <select v-model="filter.category">
+        <option
+          v-for="(option, index) in categoryOptions"
+          :key="index"
+          :value="option"
+        >
+          {{ option }}
+        </option>
+      </select>
+    </div>
+    <button @click="clearFilter" class="button_clear">Limpar</button>
   </div>
 </template>
 
@@ -36,9 +41,33 @@ export default {
       },
     };
   },
-  methods: {},
+  methods: {
+    clearFilter() {
+      this.filter.name = "";
+      this.filter.category = null;
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped="true">
+.container {
+  width: 700px;
+  justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+}
+.filter  {
+  margin-top: 5px;
+}
+.button_clear {
+  height: 80%;
+  width: 65px;
+  border-radius: 10px;
+  padding: 10;
+  font-size: 20;
+  font-weight: "bold";
+  color: white;
+  background-color: $dark-blue;
+}
 </style>
